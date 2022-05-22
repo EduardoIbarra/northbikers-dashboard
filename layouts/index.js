5d017874e0dc33471166cdd17271eadf6e19ba96
Miguel Hernandez <miguelhdzch@gmail.com>
2022-05-22 13:51:42 -0500

    Protect route to view results, fix logic to show not found page

--- layouts/index.js
+++ layouts/index.js
@@ -4,35 +4,42 @@ import Empty from './empty'
 import Layout from './layout'
 
 const Layouts = ({children}) => {
-  const router = useRouter()
-  let {pathname} = {...router}
+    const router = useRouter()
+    let {pathname} = {...router}
     console.log({pathname});
     if (['/404', '/500',].includes(pathname)) {
-    return <Centered>{children}</Centered>
-  }
-  if (
-    [
-      '/login',
-      '/contact-us-1',
-      '/create-account',
-      '/email-confirmation',
-      '/logout',
-      '/reset-password',
-      '/forgot-password',
-      '/lock-screen',
-      '/subscribe',
-      '/error-page',
-      '/coming-soon'
-    ].includes(pathname)
-  ) {
-    return <Centered>{children}</Centered>
-  } else if (
-    ['/landing', '/login-1', '/login-2', '/login-3', '/view/[routeId]'].includes(pathname)
-  ) {
-    return <Empty>{children}</Empty>
-  } else {
-    return <Layout>{children}</Layout>
-  }
+        return <Centered>{children}</Centered>
+    }
+
+    if (pathname === '/') return <Layout>{children}</Layout>
+
+    if (['/participants'].includes(pathname)) {
+        return <Layout>{children}</Layout>
+    }
+
+    if (
+        [
+            '/login',
+            '/contact-us-1',
+            '/create-account',
+            '/email-confirmation',
+            '/logout',
+            '/reset-password',
+            '/forgot-password',
+            '/lock-screen',
+            '/subscribe',
+            '/error-page',
+            '/coming-soon'
+        ].includes(pathname)
+    ) {
+        return <Centered>{children}</Centered>
+    } else if (
+        ['/landing', '/login-1', '/login-2', '/login-3', '/view/[routeId]'].includes(pathname)
+    ) {
+        return <Empty>{children}</Empty>
+    } else {
+        return <Empty>{children}</Empty>
+    }
 }
 
 export default Layouts
--- pages/view/[routeId].js
+++ pages/view/[routeId].js
@@ -3,14 +3,24 @@ import {useRouter} from "next/router";
 import {useEffect} from "react";
 import {useSetRecoilState} from "recoil";
 import {CurrentRoute} from "../../store/atoms/global";
+import {getLoggedUser} from "../../utils";
 
 const RoutePage = () => {
     const router = useRouter()
     const {routeId} = router.query
     const setCurrentRoute = useSetRecoilState(CurrentRoute);
+    const loggedUser = getLoggedUser();
+
     useEffect(() => {
         setCurrentRoute({id: routeId})
     }, [routeId])
+
+
+    // comment code if view is public
+    if(!loggedUser){
+        router.push('/login')
+        return null;
+    }
     return (
         <div className="p-12 bg-gray-50">
             <div className='bg-white p-4 rounded'>
